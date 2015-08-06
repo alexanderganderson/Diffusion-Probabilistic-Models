72b0867732758bf4481b95a667117fe730e2ddd4
Alex Anderson <aga@berkeley.edu>
2015-08-06 15:29:50 -0400

    added package import

--- load_and_sample.py
+++ load_and_sample.py
@@ -3,6 +3,7 @@ The goal of this script is to load the .pkl version of a model and run a sampler
 """
 import cPickle as pkl
 import theano.tensor as T
+import theano
 
 from fuel.streams import DataStream
 from fuel.schemes import ShuffledScheme
